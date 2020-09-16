ba2f4423f4dc1647e7866cc17aa01f5e80eba7e4
YearnSzx <yearnszx@qq.com>
2020-09-16 18:03:49 +0800

    这次添加了退出路线的东西

--- src/views/layout/Foot.vue
+++ src/views/layout/Foot.vue
@@ -8,13 +8,19 @@
         </div>
         <div></div>
         <div class="routerImg">
-            <div class="rouImg">
+            <div class="rouImg" @click="routerIsShow()">
                 <img src='@/assets/map/route_zh.png' alt="">    
             </div>
         </div>
     </div>
 
-    <div class="Router"></div>
+    <div class="Router" v-show="routerShow">
+        <div class="chooseRouter">
+            <div class="lineChoose"></div>
+            <div class="exitChoose" @click="routerIsShow()">退出路线</div>
+        </div>
+        <div class="routerShowBack"></div>
+    </div>
   </div>
 </template>
 
@@ -24,11 +30,7 @@
     name : 'foot',
     data () {
       return {
-          list:[
-              {
-                  url:''
-              }
-          ]
+          routerShow:true
       };
     },
 
@@ -39,6 +41,9 @@
     mounted() {},
 
     methods: {
+        routerIsShow:function(){
+            this.routerShow = !this.routerShow;
+        },
         positionting:function(){
             console.log(123456)
             wx.getLocation({
@@ -99,9 +104,47 @@
     width: 100%;
 }
 
+/* 选择路线 */
 .Router{
+    width: 100%;
+    display: flex;
+    align-items: flex-end;
+    position: fixed;
+    top: 42px;
+    bottom: 0;
+}
+.routerShowBack{
     width: 100%;
     height: 100%;
     background: black;
+    position: absolute;
+    opacity: 0.1;
 }
+.chooseRouter{
+    width: 90%;
+    height: 140px;
+    margin: 0 auto;
+    margin-bottom: 8%;
+    z-index: 999;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+}
+.chooseRouter>div{
+    width: 100%;
+    border-radius: 15px;
+}
+.lineChoose{
+    height: 86px;
+    background: white;
+}
+.exitChoose{
+    height: 47px;
+    background: white;
+    color: blue;
+    line-height: 47px;
+    font-size: 13px;
+}
+
 </style>
